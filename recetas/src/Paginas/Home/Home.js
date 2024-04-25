f88de36e91c4fee08c4ee87e5fd1f222abf4d825
Franco Benitez <franco.benitez@est.fi.uncoma.edu.ar>
2024-04-25 12:36:36 -0300

    Agregando CardReceta

--- recetas/src/Paginas/Home/Home.js
+++ recetas/src/Paginas/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import CardReceta from '../../Componentes/CardReceta/CardReceta';
 
 const Home = () => {
   const [recetas, setRecetas] = useState([]);
@@ -20,16 +21,11 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Lista de Recetas</h1>
-      <div className="card-container">
+      
+      <div className="grid grid-cols-3 gap-4">
+      <h1 class="col-span-full text-center text-7xl">Nuestras Recetas</h1>
         {recetas.map(receta => (
-          <Link key={receta.id} to={`/receta/${receta.id}`}>
-            <div className="card">
-              <img src={receta.imagen} alt={receta.titulo} />
-              <h2>{receta.titulo}</h2>
-              <p>{receta.descripcion}</p>
-            </div>
-          </Link>
+            <CardReceta id= {receta.id} imagen={receta.imagen} titulo={receta.titulo} descripcion={receta.descripcion}/>   
         ))}
       </div>
     </div>
